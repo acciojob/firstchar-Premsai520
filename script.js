8635e99fca3969988b6847c6448ee73162fdf24e
Premsai <142737177+Premsai520@users.noreply.github.com>
2026-07-08 23:05:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ function firstChar(text) {
     }
     return '';
 }
-
+const input = prompt();
 // Do not change the code below
 //Uncomment the following line to show the prompt popup
 //const text = prompt("Enter text:");
